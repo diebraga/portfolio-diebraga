07579ea06f54c5116fb3513f8f14917f53062284
Diego Braga <diegobraga@hertz.com>
2023-03-11 17:47:08 +0000

    fixed hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,14 +9,12 @@ const Hero = () => {
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-white" />
-        </div>
-
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             <span>Hi,</span> I'm{" "}
-            <span>Diego</span>
+            <span className="bg-gradient-to-r from-blue-500 to-pink-500 text-transparent bg-clip-text">
+              Diego
+            </span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I create awesome mobile applications
@@ -30,19 +28,20 @@ const Hero = () => {
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className="w-3 h-3 rounded-full bg-white mb-1"
-            />
-          </div>
+          <motion.div
+            animate={{
+              y: [0, 24, 0],
+              backgroundColor: ["#3182CE", "#FBB6CE", "#3182CE"],
+            }}
+            transition={{
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: "loop",
+            }}
+            className="w-10 h-10 rounded-full bg-blue-500 mb-1"
+          >
+            <div className="w-4 h-4 rounded-full bg-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+          </motion.div>
         </a>
       </div>
     </section>

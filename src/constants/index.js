88e101a39673119a4b136457f7c292bc20b21009
Diego Braga <diebraga0055@gmail.com>
2023-11-16 20:51:47 +0000

    testimonials

--- src/constants/index.js
+++ src/constants/index.js
@@ -197,27 +197,19 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      "Diego played a key role in developing the virtual event platforms that have become central to our business.",
+    name: "Humberto Yoshimoto",
+    designation: "CEO",
+    company: "Kosmos",
+    image: "/humb.jpeg",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+      "Diego's contributions to the microfrontend team have been vital to our business, significantly enhancing both our web and mobile deliveries.",
+    name: "Sebastian Karolkiewicz",
+    designation: "Senior Director Software Development at Hertz",
+    company: "Hertz",
+    image: "/seb.jpeg",
   },
 ];
 

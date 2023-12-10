1b59b1c54ea70363f470461288f1616801706bf0
Diego Braga <diebraga0055@gmail.com>
2023-12-10 16:15:40 -0300

    i

--- src/constants/index.js
+++ src/constants/index.js
@@ -162,7 +162,7 @@ const experiences = [
     company_name: "Kosmos",
     icon: kos,
     iconBg: "#000",
-    date: "Oct 2019 - Mar 2022",
+    date: "Oct 2019 - Mar 2021",
     points: [
       "Developing frontend applications using Next.js and Chakra UI.",
       "Manipulating 3D scenes using Babylon.js or Three.js to provide users with an immersive gaming experience.",
